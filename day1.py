7a52485109ef64eb92ce000834b2e00de8bc3690
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2023-06-05 23:16:39 +0600

    first commit

---
+++ day1.py
@@ -0,0 +1,9 @@
+# Data Type Check
+
+print(type(12)) # int
+print(type(12.12)) #float
+print(type("recapPythonDayOne")) #string
+print(type(80>79)) #boolean
+print(type((1,2,3))) #tuple
+print(type({12,23,34})) #set
+print(type(['Python','Data Science','ML','DL'])) #list
