3e3c604c0eebc1ba8dac8903c2564f4eed1667cc
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2023-06-06 10:45:34 +0600

    Math , Remainder , Math Function

--- day1.py
+++ day1.py
@@ -7,3 +7,24 @@ print(type(80>79)) #boolean
 print(type((1,2,3))) #tuple
 print(type({12,23,34})) #set
 print(type(['Python','Data Science','ML','DL'])) #list
+
+# Some Math 
+
+print(2**2) # 2*2 = 4
+print(3**4) # 3*3*3*3 = 81
+print(5**3) # 5*5*5 = 125
+
+print (60 // 7) # 8 .Because 7*8=56 . 
+print (89 // 13) # 6 .Because 13*6=78 . 
+print (45 // 15) # 3 .Because 45*15=3 .  
+
+#Remainder Check
+print(34 % 5) #4 
+print(45%7) #3
+print(23%8) #7
+
+# some math function 
+print(round(34.4)) #34
+print(round(23.7)) #24
+print(round(12.5)) #12
+print(round(17.6)) #18
