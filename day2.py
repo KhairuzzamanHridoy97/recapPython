7577b821bccbef9a2de89239ce960d3c4f3ce16b
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2023-06-07 23:21:01 +0600

    string

--- day2.py
+++ day2.py
@@ -8,3 +8,19 @@ black = 2001
 aurthohin = artcell - black 
 
 print(aurthohin) #black
+
+
+#string
+
+firstName = "MK"
+lastName = " Hridoy"
+
+fullName = firstName + lastName
+print(fullName) # MK Hridoy
+
+myName = "MKH"
+yourName = "TasTan"
+
+ourName = myName + ' ' + yourName
+
+print(ourName) #MKH TasTan
