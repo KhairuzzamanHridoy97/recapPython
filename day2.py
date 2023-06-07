29cbe37898ac5b38d7d8b77020fc342d09f9a4be
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2023-06-07 23:49:54 +0600

    string indexing

--- day2.py
+++ day2.py
@@ -37,3 +37,13 @@ singer = "kabir"
 age = 56
 
 print(f'Hi {singer} . How are you . I think you are {age} years old ')
+
+#String Indexing
+
+king = "The Great Hridoy"
+print(king) 
+
+print(king[0]) #t
+print(king[6]) #e
+print(king[0:3]) #The
+print(king[4:]) # Great Hridoy
