9b036d24157aee7c16f3ac072bbcc077d784281e
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2023-06-08 23:44:09 +0600

    list slicing

--- day3.py
+++ day3.py
@@ -3,3 +3,9 @@
 rupali_cart= ["apple","orange","oil","rice"]
 print(rupali_cart[0]) 
 print(rupali_cart[0:3]) # [apple , orange , oil] 
+rupali_cart[0] = "mango" # it'll replace first item
+print(rupali_cart)
+
+sonali_cart = rupali_cart[:]
+sonali_cart[1] = "jam"
+print(sonali_cart)
